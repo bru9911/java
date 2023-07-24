42026bddfefa5bfeb36aebd7d44d8be5dd879f99
Bruno Padovani Canna <60715983+bru9911@users.noreply.github.com>
2023-07-24 16:13:46 -0300

    Create continue.java

---
+++ continue.java
@@ -0,0 +1,5 @@
+for(int i=1;i<=10;i++){        //O loop é executado 10 vezes
+     if(i%2==0)
+          continue;
+     System.out.println(i+" "); //Será impressa na tela os números ímpares entre 1 e 10
+ }
